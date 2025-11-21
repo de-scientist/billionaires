9b6723c55de68a3bbb7aeabbe45a780ca9590f8d
de-scientist <gitaumark502@gmail.com>
2025-11-21 15:03:54 +0300

    export type Billionaire

---
+++ src/data/types.ts
@@ -0,0 +1,16 @@
+// types.ts
+export type Billionaire = {
+  id: string;
+  name: string;
+  netWorthUSD: number; // in dollars
+  avatar?: string;
+  description?: string;
+};
+
+export type Item = {
+  id: string;
+  name: string;
+  priceUSD: number;
+  image?: string;
+  category?: string;
+};
